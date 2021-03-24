b040d08f5230f58c90a817ef2b693bf7e4e13841
James Prickitt <jwprickitt@gmail.com>
2021-03-24 16:17:12 -0400

    base files

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+PORT = 3000
+const MongoClient = require('mongodb').MongoClient
+
+var config = require('./secrets/config');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//show index.html by default
+// app.get('/', (req,res) => {
+//     res.sendFile(__dirname+'/index.html')
+// })
+
+
+//callbacks:
+// MongoClient.connect(connectionString, {
+//   useUnifiedTopology: true
+// }, (err, client) => {
+//   if (err) return console.error(err)
+//   console.log('Connected to Database')
+// })
+
+//promises:
+MongoClient.connect(config.connectionString, { useUnifiedTopology: true })
+  .then(client => {
+      console.log('Connected to Database')
+      const db = client.db('quotemaster')
+      const quotesCollection = db.collection('quotes')
+    //   app.use(/* */)
+    //   app.get(/* */)
+      app.get('/', (req, res) => {
+          const cursor = db.collection('quotes').find().toArray()
+              .then(results => {
+              console.log(results)
+              })
+          .catch(error => console.error(error))
+
+      })
+    //   app.post(/* */)
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+        .catch(error => console.error(error))
+    })
+    //   app.listen(/* */)
+  })
+  .catch(error => console.error(error))
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+//preversew
